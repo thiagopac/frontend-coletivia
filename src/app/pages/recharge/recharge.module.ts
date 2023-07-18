8165aa22d7020ad97b34786d8252b87c5090ddc7
Thiago Pires <thiagopac@gmail.com>
2023-07-18 19:08:11 +0000

    Branch thiago

--- src/app/pages/recharge/recharge.module.ts
+++ src/app/pages/recharge/recharge.module.ts
@@ -8,9 +8,19 @@ import { PipesModule } from 'src/app/pipes/pipes.module';
 import { NgxKjuaModule } from 'ngx-kjua';
 import { CountdownModule } from 'ngx-countdown';
 import { InlineSVGModule } from 'ng-inline-svg-2';
+import {
+  MatPaginatorIntl,
+  MatPaginatorModule,
+} from '@angular/material/paginator';
+import { CustomPaginatorIntlService } from 'src/app/services/custom-paginator-intl.service';
+import { RechargeListComponent } from 'src/app/pages/recharge/recharge-list/recharge-list.component';
 
 @NgModule({
-  declarations: [RechargeOptionsComponent, CheckoutPixComponent],
+  declarations: [
+    RechargeOptionsComponent,
+    CheckoutPixComponent,
+    RechargeListComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -19,6 +29,7 @@ import { InlineSVGModule } from 'ng-inline-svg-2';
     NgxKjuaModule,
     CountdownModule,
     InlineSVGModule,
+    MatPaginatorModule,
     RouterModule.forChild([
       {
         path: 'options',
@@ -28,7 +39,14 @@ import { InlineSVGModule } from 'ng-inline-svg-2';
         path: 'checkout-pix/:uuid',
         component: CheckoutPixComponent,
       },
+      {
+        path: 'list',
+        component: RechargeListComponent,
+      },
     ]),
+  ],
+  providers: [
+    { provide: MatPaginatorIntl, useClass: CustomPaginatorIntlService },
   ],
 })
 export class RechargeModule {}
